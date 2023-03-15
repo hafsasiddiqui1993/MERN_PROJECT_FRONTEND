55b8a55849aa1ffad0dab25e60711251da9487b4
Hafsa Saad <120678121+hafsasiddiqui1993@users.noreply.github.com>
2023-03-16 02:12:39 +0500

    Update ActivityFormRead.jsx

--- pages/routes/member/ActivityFormRead.jsx
+++ pages/routes/member/ActivityFormRead.jsx
@@ -8,12 +8,12 @@ import { useState } from "react";
 function ActivityFormRead() {
   const [activity, setactivity] = useState([]);
   const [img, setImg] = useState();
-const imageUrl = 'http://localhost:8000/api/activity/member/exercise_activity"'
+const imageUrl = 'https://light-helmet.cyclic.app/api/activity/member/exercise_activity"'
   // console.log(activity)
   React.useEffect(() => {
     async function getData() {
       const res = await fetch(
-        "http://localhost:8000/api/activity/member/exercise_activity",
+        "https://light-helmet.cyclic.app/api/activity/member/exercise_activity",
         {
           method: "GET",
 
@@ -45,7 +45,7 @@ const imageUrl = 'http://localhost:8000/api/activity/member/exercise_activity"'
   async function deleteItem(id) {
     console.log(id);
     const deleteItems = await fetch(
-      "http://localhost:8000/api/activity/member/exercise_activity/delete/" +
+      "https://light-helmet.cyclic.app/api/activity/member/exercise_activity/delete/" +
         id,
       {
         method: "DELETE",
@@ -55,7 +55,7 @@ const imageUrl = 'http://localhost:8000/api/activity/member/exercise_activity"'
       }
     );
     const res = await fetch(
-      "http://localhost:8000/api/activity/member/exercise_activity",
+      "https://light-helmet.cyclic.app/api/activity/member/exercise_activity",
       {
         method: "GET",
 
@@ -93,7 +93,7 @@ const imageUrl = 'http://localhost:8000/api/activity/member/exercise_activity"'
             <td>{item.exe_ac_type}</td>
             <td>{item.exe_ac_dur}</td>
             <td>{item.exe_ac_date}</td>
-            <td> <img src={"http://localhost:8000/"+item.exe_ac_img} width={80}/></td>
+            <td> <img src={"https://light-helmet.cyclic.app/"+item.exe_ac_img} width={80}/></td>
               <td>
             
             </td>
